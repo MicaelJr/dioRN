4ee7ad3e4f6d68a604e803d8ec1d87dad9598c9f
MicaelJr <junio_micael@hotmail.com>
2022-05-26 17:24:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Pressable,
+  Linking,
+} from 'react-native';
+
+const colorGitHub = '#010409';
+const colorFontGithub = '#C9D1D9';
+const colorNickNameGithub = '#4F565E';
+
+const imgGitHub = 'https://avatars.githubusercontent.com/u/43708135?v=4';
+const urlToMyGithub = 'https://github.com/MicaelJr';
+
+const App = () => {
+  const handlePressGoToGithub = async () => {
+    console.log('verificando link');
+    const res = await Linking.canOpenURL(urlToMyGithub);
+    if (res) {
+      console.log('link aprovado');
+      console.log('abrindo link');
+      await Linking.openURL(urlToMyGithub);
+    }
+  };
+
+  return (
+    <SafeAreaView style={style.container}>
+      <StatusBar backgroundColor={colorGitHub} barStyle="light-content" />
+      <View style={style.content}>
+        <Image
+          accessibilityLabel="Micael no quarto com fundo amarelo"
+          style={style.avatar}
+          source={{uri: imgGitHub}}
+        />
+        <Text
+          accessibilityLabel="Nome: Micael Silva"
+          style={[style.defaultText, style.name]}>
+          Micael Silva
+        </Text>
+        <Text
+          accessibilityLabel="NickName: Micael Jr"
+          style={[style.defaultText, style.nickname]}>
+          MicaelJr
+        </Text>
+        <Text
+          accessibilityLabel="Descrição: Software Developer | Full Stack Developer | Mobile Developer"
+          style={[style.defaultText, style.description]}>
+          Software Developer | Full Stack Developer | Mobile Developer
+        </Text>
+        <Pressable onPress={handlePressGoToGithub}>
+          <View style={style.button}>
+            <Text style={[style.defaultText, style.textButton]}>
+              Open in Github
+            </Text>
+          </View>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: colorGitHub,
+    flex: 1, //expandir para a tela inteira
+    justifyContent: 'center',
+    alignItems: 'center',
+    //flexDirection: 'row',
+  },
+  content: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  avatar: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  defaultText: {
+    color: colorFontGithub,
+  },
+  name: {
+    marginTop: 20,
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  nickname: {
+    color: colorNickNameGithub,
+    fontSize: 18,
+  },
+  description: {
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: colorNickNameGithub,
+    borderRadius: 10,
+    padding: 20,
+  },
+  textButton: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
